edc2a971f359c558e22b3869fc5bc7851d48ae31
Anton P. <made1of1fire@gmail.com>
2024-05-27 20:32:34 +0500

    feat(ConcurrentCollections): added PageVisitors -> A random address is generated and each of the threads (newFixedThreadPool(3)) records a visit to this address in the thread-safe ConcurrentHashMap collection

---
+++ Multithreads_practice/src/ConcurrentCollections/PageVisitors/PageVisitCounter.java
@@ -0,0 +1,20 @@
+package ConcurrentCollections.PageVisitors;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class PageVisitCounter {
+    private static ConcurrentHashMap<String, Integer> pageCounter = new ConcurrentHashMap<>();
+
+    public static void incrementPageVisit(String url) {
+        pageCounter.merge(url, 1, Integer::sum);
+    }
+
+    public static void getStats() {
+        System.out.println("\nPage stats:");
+        System.out.println("All pages - " + pageCounter.values().stream().reduce(Integer::sum).get());
+        for (Map.Entry<String, Integer> entry : pageCounter.entrySet()) {
+            System.out.println(entry.getKey() + " - " + entry.getValue());
+        }
+    }
+}
---
+++ Multithreads_practice/src/ConcurrentCollections/PageVisitors/PageVisitors.java
@@ -0,0 +1,28 @@
+package ConcurrentCollections.PageVisitors;
+
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class PageVisitors {
+    private static ExecutorService service = Executors.newFixedThreadPool(3);
+
+    public static void main(String[] args) throws InterruptedException {
+        String[] urls = {"www.first.com", "www.second.com",
+                "www.third.com", "www.google.com",
+                "www.microsoft.com", "www.apple.com"};
+
+        for (int i = 0; i < 10_000; i++) {
+            service.submit(() -> {
+                int randomInt = new Random().nextInt(urls.length);
+                System.out.println(Thread.currentThread().getName() + " added new url - " + urls[randomInt]);
+                PageVisitCounter.incrementPageVisit(urls[randomInt]);
+            });
+        }
+        service.shutdown();
+        service.awaitTermination(5_000, TimeUnit.MILLISECONDS);
+
+        PageVisitCounter.getStats();
+    }
+}
